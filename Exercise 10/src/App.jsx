7a5196f65331ab043dafb3cda90c0e534d45e40f
sammed384 <sammed384@gmail.com>
2025-11-03 11:09:10 +0300

    corrected the error and added the image

--- Exercise 10/src/App.jsx
+++ Exercise 10/src/App.jsx
@@ -1,21 +1,16 @@
-
-import UserList from "./UserList"
-
-
+import UserList from "./UserList";
 function App() {
-  const users= [
-    {id:1, name:"samatar", email:"sammed384@gmail.com"},
-    {id:2, name:"mohamed", email:"mohamed@gmail.com"}
-  ]
+  const users = [
+    { id: 1, name: "samatar", email: "sammed384@gmail.com" },
+    { id: 2, name: "mohamed", email: "mohamed@gmail.com" },
+  ];
   // const users1= [1,"samatar","sammed384"]
-  
 
   return (
     <>
-     
-    <UserList users={users}/>
+      <UserList users={users} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
